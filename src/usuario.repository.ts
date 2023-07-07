f81c0fa7c121bbb342e3eab953daf78f2bb7077b
GuilhermeHS2 <guilhermehsantos00@gmail.com>
2023-07-06 22:06:04 -0300

    Cadastrando um Usuario

---
+++ src/usuario.repository.ts
@@ -0,0 +1,8 @@
+export class UsuarioRepository {
+  private usuarios = [];
+  //estou criando uma lista para salvar os usuarios
+  async salvar(usuario) {
+    this.usuarios.push(usuario);
+    console.log(this.usuarios);
+  }
+}
