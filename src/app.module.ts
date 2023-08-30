e99ce6af3f262b3a7ba50b10c39198cd31d1fde9
GuilhermeHS2 <guilhermehsantos00@gmail.com>
2023-08-30 20:51:09 -0300

    Adicionando Banco

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { PostgresConfigService } from './config/postgres.config.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    UsuarioModule,
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService]
+    })
+  ],
 })
 export class AppModule {}
