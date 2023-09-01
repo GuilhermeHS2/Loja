ec6584671b7865381117ec005064209cbfaf2ffa
GuilhermeHS2 <guilhermehsantos00@gmail.com>
2023-09-01 16:05:46 -0300

    configuracao de service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,21 @@ import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
 import { PostgresConfigService } from './config/postgres.config.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 
 @Module({
   imports: [
     UsuarioModule,
+    ConfigModule.forRoot({
+      isGlobal:true,      
+
+
+    }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
-      inject: [PostgresConfigService]
+      inject: [PostgresConfigService
+      ]
     })
   ],
 })
