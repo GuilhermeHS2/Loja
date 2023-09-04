8d3d90760cfbccfc47219b5854c7fb43350f08b8
GuilhermeHS2 <guilhermehsantos00@gmail.com>
2023-09-04 20:43:55 -0300

    Adicionando produto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProdutoModule } from './produto/produto.module';
 import { UsuarioModule } from './usuario/usuario.module';
 import { PostgresConfigService } from './config/postgres.config.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-
 
 @Module({
   imports: [
     UsuarioModule,
+    ProdutoModule,
     ConfigModule.forRoot({
-      isGlobal:true,      
-
-
+      isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
       useClass: PostgresConfigService,
-      inject: [PostgresConfigService
-      ]
-    })
+      inject: [PostgresConfigService],
+    }),
   ],
 })
 export class AppModule {}
