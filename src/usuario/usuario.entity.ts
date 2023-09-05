7f3046aac516ea2728d5517830a7f277dca1736f
GuilhermeHS2 <guilhermehsantos00@gmail.com>
2023-09-04 21:56:47 -0300

    Criando tabelas no banco

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -1,7 +1,28 @@
+import { Delete } from '@nestjs/common';
+import{Entity, Column, CreateDateColumn, UpdateDateColumn, DeleteDateColumn, PrimaryGeneratedColumn} from 'typeorm'
+
+@Entity({name: 'usuarios'})
 export class UsuarioEntity {
+
+    @PrimaryGeneratedColumn('uuid')
     id: string;
+
+    @Column({name:'nome', length: 100, nullable: false})
     nome: string;
+
+    @Column({name:'email', length: 50, nullable: false})
     email:string;
+    
+    @Column({name:'senha',length: 255, nullable:false})
     senha:string;
+
+    @CreateDateColumn({name: 'created_at'})
+    createdAt: string;
+
+    @UpdateDateColumn({name: 'updated_at'})
+    updateAt: string;
+
+    @DeleteDateColumn({name: 'deleted_at'})
+    deletedAt: string;
 
 }
